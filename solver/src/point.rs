3a31a6225297511b7343ff88e38ad004426983eb
Bryan Lee <bryanmylee@gmail.com>
2022-11-02 20:44:17 +1100

    feat: add and eq for point

--- solver/src/point.rs
+++ solver/src/point.rs
@@ -1,5 +1,18 @@
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Point<T> {
     pub x: T,
     pub y: T,
+}
+
+use std::ops::Add;
+
+impl<T: Add<Output = T>> Add for Point<T> {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Point<T> {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
 }
