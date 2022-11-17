0fed9f3ed1a988e48b232f3bbd8eda057d8eacf3
Bryan Lee <bryanmylee@gmail.com>
2022-11-17 23:32:20 +1100

    feat: srs kick table

--- solver/src/config.rs
+++ solver/src/config.rs
@@ -7,3 +7,118 @@ pub enum RotationSystem {
 pub struct Config {
     pub rotation_system: RotationSystem,
 }
+
+pub mod srs {
+    use crate::{piece::PieceKind, point::Point, rotation::Orientation};
+
+    pub fn kick_table(
+        piece_kind: &PieceKind,
+        from: &Orientation,
+        to: &Orientation,
+    ) -> Option<[Point<isize>; 4]> {
+        match piece_kind {
+            PieceKind::I => match (from, to) {
+                (Orientation::North, Orientation::East) => Some([
+                    Point { x: -2, y: 0 },
+                    Point { x: 1, y: 0 },
+                    Point { x: -2, y: -1 },
+                    Point { x: 1, y: 2 },
+                ]),
+                (Orientation::East, Orientation::North) => Some([
+                    Point { x: 2, y: 0 },
+                    Point { x: -1, y: 0 },
+                    Point { x: 2, y: 1 },
+                    Point { x: -1, y: -2 },
+                ]),
+                (Orientation::East, Orientation::South) => Some([
+                    Point { x: -1, y: 0 },
+                    Point { x: 2, y: 0 },
+                    Point { x: -1, y: 2 },
+                    Point { x: 2, y: -1 },
+                ]),
+                (Orientation::South, Orientation::East) => Some([
+                    Point { x: 1, y: 0 },
+                    Point { x: -2, y: 0 },
+                    Point { x: 1, y: -2 },
+                    Point { x: -2, y: 1 },
+                ]),
+                (Orientation::South, Orientation::West) => Some([
+                    Point { x: 2, y: 0 },
+                    Point { x: -1, y: 0 },
+                    Point { x: 2, y: 1 },
+                    Point { x: -1, y: -2 },
+                ]),
+                (Orientation::West, Orientation::South) => Some([
+                    Point { x: -2, y: 0 },
+                    Point { x: 1, y: 0 },
+                    Point { x: -2, y: -1 },
+                    Point { x: 1, y: 2 },
+                ]),
+                (Orientation::West, Orientation::North) => Some([
+                    Point { x: 1, y: 0 },
+                    Point { x: -2, y: 0 },
+                    Point { x: 1, y: -2 },
+                    Point { x: -2, y: 1 },
+                ]),
+                (Orientation::North, Orientation::West) => Some([
+                    Point { x: -1, y: 0 },
+                    Point { x: 2, y: 0 },
+                    Point { x: -1, y: 2 },
+                    Point { x: 2, y: -1 },
+                ]),
+                _ => None,
+            },
+            _ => match (from, to) {
+                (Orientation::North, Orientation::East) => Some([
+                    Point { x: -1, y: 0 },
+                    Point { x: -1, y: 1 },
+                    Point { x: 0, y: -2 },
+                    Point { x: -1, y: -2 },
+                ]),
+                (Orientation::East, Orientation::North) => Some([
+                    Point { x: 1, y: 0 },
+                    Point { x: 1, y: -1 },
+                    Point { x: 0, y: 2 },
+                    Point { x: 1, y: 2 },
+                ]),
+                (Orientation::East, Orientation::South) => Some([
+                    Point { x: 1, y: 0 },
+                    Point { x: 1, y: -1 },
+                    Point { x: 0, y: 2 },
+                    Point { x: 1, y: 2 },
+                ]),
+                (Orientation::South, Orientation::East) => Some([
+                    Point { x: -1, y: 0 },
+                    Point { x: -1, y: 1 },
+                    Point { x: 0, y: -2 },
+                    Point { x: -1, y: -2 },
+                ]),
+                (Orientation::South, Orientation::West) => Some([
+                    Point { x: 1, y: 0 },
+                    Point { x: 1, y: 1 },
+                    Point { x: 0, y: -2 },
+                    Point { x: 1, y: -2 },
+                ]),
+                (Orientation::West, Orientation::South) => Some([
+                    Point { x: -1, y: 0 },
+                    Point { x: -1, y: -1 },
+                    Point { x: 0, y: 2 },
+                    Point { x: -1, y: 2 },
+                ]),
+                (Orientation::West, Orientation::North) => Some([
+                    Point { x: -1, y: 0 },
+                    Point { x: -1, y: -1 },
+                    Point { x: 0, y: 2 },
+                    Point { x: -1, y: 2 },
+                ]),
+                (Orientation::North, Orientation::West) => Some([
+                    Point { x: 1, y: 0 },
+                    Point { x: 1, y: 1 },
+                    Point { x: 0, y: -2 },
+                    Point { x: 1, y: -2 },
+                ]),
+                _ => None,
+            },
+        }
+    }
+}
