29ebea92266b5c6f49fd6de0fa57bf5145057688
Bryan Lee <bryanmylee@gmail.com>
2022-12-17 11:15:36 +0800

    feat: async import main page to bootstrap solver import

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+{#await import('./app.svelte')}
+  <p>Loading...</p>
+{:then App}
+  <svelte:component this={App.default} />
+{/await}
---
+++ app/src/routes/app.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import {Solver} from 'perfect-clear';
+  
+  const solver = Solver.new();
+  console.log(solver);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
