8e9704c62e8c15f4502e1caa93a4c3962b8b7368
Bryan Lee <bryanmylee@gmail.com>
2022-12-17 10:10:59 +0800

    build: import wasm into app

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,8 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), wasm(), topLevelAwait()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
